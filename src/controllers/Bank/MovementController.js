9b021a0c422e67c12501269aea495e81701db100
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-02-09 21:54:10 -0400

    update account controller and movement controller

--- src/controllers/Bank/MovementController.js
+++ src/controllers/Bank/MovementController.js
@@ -1,88 +1,87 @@
-const { Movement, BusinessAccount } = require("../../models/Bank");
+const { Movement, BusinessAccount } = require('../../models/Bank');
 
 const MovementController = {
-  getAll: async (req, res) => {
-    const movements = await Movement.find({});
+	getAll: async (req, res) => {
+		const movements = await Movement.find({}, [
+			'date',
+			'reference',
+			'description',
+			'amount',
+			'conciliate',
+			'invoices'
+		]);
 
-    return res.status(200).json(movements);
-  },
+		return res.status(200).json(movements);
+	},
 
-  get: async (req, res) => {
-    const { movementId } = req.params;
-    const movement = await Movement.findById(movementId)
-      .populate("account", "bank", {
-        populate: "account",
-        select: ["bank", "type"]
-      })
-      .populate("invoices", [
-        "number",
-        "address",
-        "dateEmit",
-        "paid",
-        "paymentType",
-        "type",
-        "user"
-      ]);
+	get: async (req, res) => {
+		const { movementId } = req.params;
+		const movement = await Movement.findById(movementId).populate('account', [
+			'name',
+			'bank',
+			'number',
+			'type'
+		]);
 
-    if (movement === null) {
-      return res
-        .status(404)
-        .json({ success: false, message: "No encontrado." });
-    } else {
-      return res.status(200).json(movement);
-    }
-  },
+		if (movement === null) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'No encontrado.' });
+		} else {
+			return res.status(200).json(movement);
+		}
+	},
 
-  new: async (req, res) => {
-    const { accountId, ...data } = req.body;
-    const account = BusinessAccount.findById(accoundId);
+	new: async (req, res) => {
+		const { accountId, ...data } = req.body;
+		const account = BusinessAccount.findById(accoundId);
 
-    if (account === null) {
-      return res
-        .status(404)
-        .json({ success: false, message: "No encontrado." });
-    } else {
-      const newMovement = new Movement({ account, ...data });
-      await newMovement.save();
-      account.movements.push(newMovement);
-      await account.save();
+		if (account === null) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'No encontrado.' });
+		} else {
+			const newMovement = new Movement({ account, ...data });
+			await newMovement.save();
+			account.movements.push(newMovement);
+			await account.save();
 
-      return res
-        .status(201)
-        .json({ success: true, message: "Registrado con exito!" });
-    }
-  },
+			return res
+				.status(201)
+				.json({ success: true, message: 'Registrado con exito!' });
+		}
+	},
 
-  update: async (req, res) => {
-    const { movementId } = req.params;
-    const data = req.body;
-    const movement = await Movement.findByIdAndUpdate(movementId, data);
+	update: async (req, res) => {
+		const { movementId } = req.params;
+		const data = req.body;
+		const movement = await Movement.findByIdAndUpdate(movementId, data);
 
-    if (movement === null) {
-      return res
-        .status(404)
-        .json({ success: false, message: "No encontrado." });
-    } else {
-      return res
-        .status(200)
-        .json({ success: true, message: "Actualizado con exito!" });
-    }
-  },
+		if (movement === null) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'No encontrado.' });
+		} else {
+			return res
+				.status(200)
+				.json({ success: true, message: 'Actualizado con exito!' });
+		}
+	},
 
-  delete: async (req, res) => {
-    const { movementId } = req.params;
-    const movement = await Movement.findByIdAndRemove(movementId);
+	delete: async (req, res) => {
+		const { movementId } = req.params;
+		const movement = await Movement.findByIdAndRemove(movementId);
 
-    if (movement === null) {
-      return res
-        .status(404)
-        .json({ success: false, message: "No encontrado." });
-    } else {
-      return res
-        .status(200)
-        .json({ success: true, message: "Eliminado con exito!" });
-    }
-  }
+		if (movement === null) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'No encontrado.' });
+		} else {
+			return res
+				.status(200)
+				.json({ success: true, message: 'Eliminado con exito!' });
+		}
+	}
 };
 
 module.exports = MovementController;
