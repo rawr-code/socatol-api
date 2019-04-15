7d1dbbd4533272a7753164db0c8bfb0cb5281d22
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-04-14 21:32:31 -0400

    add schema and resolvers for Warehouse

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,22 +1,60 @@
+// Models
+const Warehouse = require('./models/Warehouse');
+
 const resolvers = {
   Query: {
-    getCliente: () => {
-      return {
-        nombre: 'Get Client',
-        apellido: 'String',
-        empresa: 'String',
-        email: 'String'
-      };
+    // Warehouse Queries
+    getWarehouse: (root, { id }) => {
+      return new Promise((resolve, object) => {
+        Warehouse.findById(id, (error, warehouse) => {
+          if (error) rejects(error);
+          else resolve(warehouse);
+        });
+      });
+    },
+    getWarehouses: (root, { limit, offset }) => {
+      return (warehouses = Warehouse.find({})
+        .limit(limit)
+        .skip(offset));
     }
   },
   Mutation: {
-    crearCliente: () => {
-      return {
-        nombre: 'New Client',
-        apellido: 'String',
-        empresa: 'String',
-        email: 'String'
-      };
+    // Warehouse Mutations
+    newWarehouse: (root, { input }) => {
+      const { name, description, active } = input;
+
+      const warehouse = new Warehouse({
+        name,
+        description,
+        active
+      });
+
+      return new Promise((resolve, object) => {
+        warehouse.save(error => {
+          if (error) rejects(error);
+          else resolve(warehouse);
+        });
+      });
+    },
+    updateWarehouse: (root, { input }) => {
+      return new Promise((resolve, warehouse) => {
+        Warehouse.findOneAndUpdate(
+          { _id: input.id },
+          input,
+          (error, warehouse) => {
+            if (error) rejects(error);
+            else resolve(warehouse);
+          }
+        );
+      });
+    },
+    deleteWarehouse: (root, { id }) => {
+      return new Promise((resolve, warehouse) => {
+        Warehouse.findOneAndDelete({ _id: id }, error => {
+          if (error) rejects(error);
+          else resolve('Se elimino correctamente');
+        });
+      });
     }
   }
 };
