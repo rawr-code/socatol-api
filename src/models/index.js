dec43bfecbdda206a0f849db901cff6f85be832d
Emmanuel Villegas <emmanuelvillegas01@gmail.com>
2019-05-04 18:49:19 -0400

    module models

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const Configuration = require('./Configuration');
+const User = require('./User');
+const Invoice = require('./Invoice');
+const Warehouse = require('./Warehouse');
+const Product = require('./Product');
+const PersonalInformation = require('./PersonalInformation');
+const BankAccount = require('./BankAccount');
+
+module.exports = {
+  Configuration,
+  User,
+  Invoice,
+  Warehouse,
+  Product,
+  PersonalInformation,
+  BankAccount
+};
